a1e5f60320198f18afc689e52210ffb65438e15e
Adrian Molofsky <molofsky@stanford.edu>
2023-01-20 18:33:00 -0800

    Add files via upload

---
+++ substr.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdio.h>
+
+char *substr(const char *s, char start, char end, char result[]) {
+    result[0] = '\0';
+    char *first = strchr(s, start);
+    if (!first) {
+        return result;
+    }
+    char *last = strchr(first + 1, end);
+    if (!last) {
+        return result;
+    }
+
+    int len = last - first;
+    strncpy(result, first, len + 1);
+    result[len + 1] = '\0';
+}
+
+main() {
+    char result[] = "";
+    char * word = "Milan";
+    char start = 'i';
+    char stop = 'n';
+    substr(word, start, stop, result);
+    printf("%s", result);
+    return 0;
+}
